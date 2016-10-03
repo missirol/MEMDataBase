eec47665d7d9444ad2084f13e81c9a01a47491d6
Karim <karim.morabit@kit.edu>
2016-10-03 14:58:36 +0200

    added script ro replace headers for standaloen use

---
+++ MEMDataBase/test/replaceIncludes.py
@@ -0,0 +1,31 @@
+import sys
+import os
+import glob
+
+print os.getcwd()
+cwd=os.getcwd()
+
+
+
+filelist=[]
+
+filelist+=glob.glob("MEMDataBase/src/*.cc")
+filelist+=glob.glob("MEMDataBase/interface/*.h")
+print filelist
+
+for fn in filelist:
+  newlist=[]
+  f=open(fn,"r")
+  oldlist=list(f)
+  for line in oldlist:
+    newline=line
+    if "#include \"MEMDataBase/MEMDataBase/" in line:
+      newline=line.replace("#include \"MEMDataBase/", "#include \""+cwd+"/")
+    newlist.append(newline)
+
+  f.close()
+  newfile=open(fn,"w")
+  for line in newlist:
+    newfile.write(line)
+  newfile.close()
+
