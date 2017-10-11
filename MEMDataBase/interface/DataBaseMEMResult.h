f811b6c1fe6f929d842b6f3a14b1ee94d0531bf6
Carmen <carmen.diez@desy.de>
2017-10-11 17:32:24 +0200

    Modified files including MEM value for JES sources

--- MEMDataBase/interface/DataBaseMEMResult.h
+++ MEMDataBase/interface/DataBaseMEMResult.h
@@ -14,6 +14,63 @@ public:
   double p_sig;
   double p_bkg;
 
+  //Systematic variations for the likelihood ratio
+
+  double p_AbsoluteStatUp;
+  double p_AbsoluteStatDown;
+  double p_AbsoluteScaleUp;
+  double p_AbsoluteScaleDown;
+  double p_AbsoluteFlavMapUp;
+  double p_AbsoluteFlavMapDown;
+  double p_AbsoluteMPFBiasUp;
+  double p_AbsoluteMPFBiasDown;
+  double p_FragmentationUp;
+  double p_FragmentationDown;
+  double p_SinglePionECALUp;
+  double p_SinglePionECALDown;
+  double p_SinglePionHCALUp;
+  double p_SinglePionHCALDown;
+  double p_FlavorQCDUp;
+  double p_FlavorQCDDown;
+  double p_TimePtEtaUp;
+  double p_TimePtEtaDown;
+  double p_RelativeJEREC1Up;
+  double p_RelativeJEREC1Down;
+  double p_RelativeJEREC2Up;
+  double p_RelativeJEREC2Down;
+  double p_RelativeJERHFUp;
+  double p_RelativeJERHFDown;
+  double p_RelativePtBBUp;
+  double p_RelativePtBBDown;
+  double p_RelativePtEC1Up;
+  double p_RelativePtEC1Down;
+  double p_RelativePtEC2Up;
+  double p_RelativePtEC2Down;
+  double p_RelativePtHFUp;
+  double p_RelativePtHFDown;
+  double p_RelativeFSRUp;
+  double p_RelativeFSRDown;
+  double p_RelativeStatFSRUp;
+  double p_RelativeStatFSRDown;
+  double p_RelativeStatECUp;
+  double p_RelativeStatECDown;
+  double p_RelativeStatHFUp;
+  double p_RelativeStatHFDown;
+  double p_PileUpDataMCUp;
+  double p_PileUpDataMCDown;
+  double p_PileUpPtRefUp;
+  double p_PileUpPtRefDown;
+  double p_PileUpPtBBUp;
+  double p_PileUpPtBBDown;
+  double p_PileUpPtEC1Up;
+  double p_PileUpPtEC1Down;
+  double p_PileUpPtEC2Up;
+  double p_PileUpPtEC2Down;
+  double p_PileUpPtHFUp;
+  double p_PileUpPtHFDown;
+  double p_RelativeBalUp;
+  double p_RelativeBalDown;
+
   //Integration uncertainties of the probabilities
   double p_err_sig;
   double p_err_bkg;
--- MEMDataBase/interface/DataBaseSample.h
+++ MEMDataBase/interface/DataBaseSample.h
@@ -27,7 +27,7 @@ public:
   
   DataBaseMEMResult GetMEMResult(const Long64_t runNumber, const Long64_t lumiSection, const Long64_t eventNumber);
   
-  bool AddEvent(Long64_t runNumber, Long64_t lumiSection, Long64_t eventNumber, Double_t p, Double_t p_sig, Double_t p_bkg, Double_t p_err_sig, Double_t p_err_bkg, Double_t n_perm_sig, Double_t n_perm_bkg);
+  bool AddEvent(Long64_t runNumber, Long64_t lumiSection, Long64_t eventNumber, Double_t p, Double_t p_sig, Double_t p_bkg, Double_t p_AbsoluteStatUp, Double_t p_AbsoluteStatDown, Double_t p_AbsoluteScaleUp, Double_t p_AbsoluteScaleDown, Double_t p_AbsoluteFlavMapUp, Double_t p_AbsoluteFlavMapDown, Double_t p_AbsoluteMPFBiasUp, Double_t p_AbsoluteMPFBiasDown, Double_t p_FragmentationUp, Double_t p_FragmentationDown, Double_t p_SinglePionECALUp, Double_t p_SinglePionECALDown, Double_t p_SinglePionHCALUp, Double_t p_SinglePionHCALDown, Double_t p_FlavorQCDUp, Double_t p_FlavorQCDDown, Double_t p_TimePtEtaUp, Double_t p_TimePtEtaDown, Double_t p_RelativeJEREC1Up, Double_t p_RelativeJEREC1Down, Double_t p_RelativeJEREC2Up, Double_t p_RelativeJEREC2Down, Double_t p_RelativeJERHFUp, Double_t p_RelativeJERHFDown, Double_t p_RelativePtBBUp, Double_t p_RelativePtBBDown, Double_t p_RelativePtEC1Up, Double_t p_RelativePtEC1Down, Double_t p_RelativePtEC2Up, Double_t p_RelativePtEC2Down, Double_t p_RelativePtHFUp, Double_t p_RelativePtHFDown, Double_t p_RelativeFSRUp, Double_t p_RelativeFSRDown, Double_t p_RelativeStatFSRUp, Double_t p_RelativeStatFSRDown, Double_t p_RelativeStatECUp, Double_t p_RelativeStatECDown, Double_t p_RelativeStatHFUp, Double_t p_RelativeStatHFDown, Double_t p_PileUpDataMCUp, Double_t p_PileUpDataMCDown, Double_t p_PileUpPtRefUp, Double_t p_PileUpPtRefDown, Double_t p_PileUpPtBBUp, Double_t p_PileUpPtBBDown, Double_t p_PileUpPtEC1Up, Double_t p_PileUpPtEC1Down, Double_t p_PileUpPtEC2Up, Double_t p_PileUpPtEC2Down, Double_t p_PileUpPtHFUp, Double_t p_PileUpPtHFDown, Double_t p_RelativeBalUp, Double_t p_RelativeBalDown, Double_t p_err_sig, Double_t p_err_bkg, Double_t n_perm_sig, Double_t n_perm_bkg);
   bool SaveDataBase();
 private:
 
@@ -47,7 +47,7 @@ private:
   
   TString GetFileNameForEvent(const Long64_t runNumber, const Long64_t lumiSection, const Long64_t eventNumber);
 //   bool GetRelevantRunAndLumi(const Long64_t lumiSection, const Long64_t eventNumber);
-  void AddEventToTree(Long64_t runNumber, Long64_t lumiSection, Long64_t eventNumber, Double_t p, Double_t p_sig, Double_t p_bkg, Double_t p_err_sig, Double_t p_err_bkg, Double_t n_perm_sig, Double_t n_perm_bkg);
+  void AddEventToTree(Long64_t runNumber, Long64_t lumiSection, Long64_t eventNumber, Double_t p, Double_t p_sig, Double_t p_bkg, Double_t p_AbsoluteStatUp, Double_t p_AbsoluteStatDown, Double_t p_AbsoluteScaleUp, Double_t p_AbsoluteScaleDown, Double_t p_AbsoluteFlavMapUp, Double_t p_AbsoluteFlavMapDown, Double_t p_AbsoluteMPFBiasUp, Double_t p_AbsoluteMPFBiasDown, Double_t p_FragmentationUp, Double_t p_FragmentationDown, Double_t p_SinglePionECALUp, Double_t p_SinglePionECALDown, Double_t p_SinglePionHCALUp, Double_t p_SinglePionHCALDown, Double_t p_FlavorQCDUp, Double_t p_FlavorQCDDown, Double_t p_TimePtEtaUp, Double_t p_TimePtEtaDown, Double_t p_RelativeJEREC1Up, Double_t p_RelativeJEREC1Down, Double_t p_RelativeJEREC2Up, Double_t p_RelativeJEREC2Down, Double_t p_RelativeJERHFUp, Double_t p_RelativeJERHFDown, Double_t p_RelativePtBBUp, Double_t p_RelativePtBBDown, Double_t p_RelativePtEC1Up, Double_t p_RelativePtEC1Down, Double_t p_RelativePtEC2Up, Double_t p_RelativePtEC2Down, Double_t p_RelativePtHFUp, Double_t p_RelativePtHFDown, Double_t p_RelativeFSRUp, Double_t p_RelativeFSRDown, Double_t p_RelativeStatFSRUp, Double_t p_RelativeStatFSRDown, Double_t p_RelativeStatECUp, Double_t p_RelativeStatECDown, Double_t p_RelativeStatHFUp, Double_t p_RelativeStatHFDown, Double_t p_PileUpDataMCUp, Double_t p_PileUpDataMCDown, Double_t p_PileUpPtRefUp, Double_t p_PileUpPtRefDown, Double_t p_PileUpPtBBUp, Double_t p_PileUpPtBBDown, Double_t p_PileUpPtEC1Up, Double_t p_PileUpPtEC1Down, Double_t p_PileUpPtEC2Up, Double_t p_PileUpPtEC2Down, Double_t p_PileUpPtHFUp, Double_t p_PileUpPtHFDown, Double_t p_RelativeBalUp, Double_t p_RelativeBalDown, Double_t p_err_sig, Double_t p_err_bkg, Double_t n_perm_sig, Double_t n_perm_bkg);
   bool RemoveEventCollection(const Long64_t runNumber_, const Long64_t minLumi_, const Long64_t maxLumi_,const Long64_t minEvent_, const Long64_t maxEvent_);
   
   bool OpenTree(TString filename);
@@ -71,6 +71,61 @@ private:
   Double_t br_p;
   Double_t br_p_sig;
   Double_t br_p_bkg;
+  Double_t br_p_AbsoluteStatUp;
+  Double_t br_p_AbsoluteStatDown;
+  Double_t br_p_AbsoluteScaleUp;
+  Double_t br_p_AbsoluteScaleDown;
+  Double_t br_p_AbsoluteFlavMapUp;
+  Double_t br_p_AbsoluteFlavMapDown;
+  Double_t br_p_AbsoluteMPFBiasUp;
+  Double_t br_p_AbsoluteMPFBiasDown;
+  Double_t br_p_FragmentationUp;
+  Double_t br_p_FragmentationDown;
+  Double_t br_p_SinglePionECALUp;
+  Double_t br_p_SinglePionECALDown;
+  Double_t br_p_SinglePionHCALUp;
+  Double_t br_p_SinglePionHCALDown;
+  Double_t br_p_FlavorQCDUp;
+  Double_t br_p_FlavorQCDDown;
+  Double_t br_p_TimePtEtaUp;
+  Double_t br_p_TimePtEtaDown;
+  Double_t br_p_RelativeJEREC1Up;
+  Double_t br_p_RelativeJEREC1Down;
+  Double_t br_p_RelativeJEREC2Up;
+  Double_t br_p_RelativeJEREC2Down;
+  Double_t br_p_RelativeJERHFUp;
+  Double_t br_p_RelativeJERHFDown;
+  Double_t br_p_RelativePtBBUp;
+  Double_t br_p_RelativePtBBDown;
+  Double_t br_p_RelativePtEC1Up;
+  Double_t br_p_RelativePtEC1Down;
+  Double_t br_p_RelativePtEC2Up;
+  Double_t br_p_RelativePtEC2Down;
+  Double_t br_p_RelativePtHFUp;
+  Double_t br_p_RelativePtHFDown;
+  Double_t br_p_RelativeFSRUp;
+  Double_t br_p_RelativeFSRDown;
+  Double_t br_p_RelativeStatFSRUp;
+  Double_t br_p_RelativeStatFSRDown;
+  Double_t br_p_RelativeStatECUp;
+  Double_t br_p_RelativeStatECDown;
+  Double_t br_p_RelativeStatHFUp;
+  Double_t br_p_RelativeStatHFDown;
+  Double_t br_p_PileUpDataMCUp;
+  Double_t br_p_PileUpDataMCDown;
+  Double_t br_p_PileUpPtRefUp;
+  Double_t br_p_PileUpPtRefDown;
+  Double_t br_p_PileUpPtBBUp;
+  Double_t br_p_PileUpPtBBDown;
+  Double_t br_p_PileUpPtEC1Up;
+  Double_t br_p_PileUpPtEC1Down;
+  Double_t br_p_PileUpPtEC2Up;
+  Double_t br_p_PileUpPtEC2Down;
+  Double_t br_p_PileUpPtHFUp;
+  Double_t br_p_PileUpPtHFDown;
+  Double_t br_p_RelativeBalUp;
+  Double_t br_p_RelativeBalDown;
+
   Double_t br_p_err_sig;
   Double_t br_p_err_bkg;
   Double_t br_n_perm_sig;
